2aeb20474e9ef698742cdecc8424e40146c3d847
whitedeath501 <whitedeath501@proton.me>
2026-06-02 19:14:30 -0300

    Fix case sensitivity in User model import

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 const { sendAdminNotification, sendWelcomeEmail, sendPasswordResetEmail } = require('../utils/mailer');
 
 // REGISTER
